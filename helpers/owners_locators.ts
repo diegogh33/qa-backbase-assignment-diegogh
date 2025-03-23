433ad9d82cce5bbd1ab93d0c94a49a2281dceb7a
DESKTOP-BEGIBOB\Diego <dvich33@gmail.com>
2025-03-23 13:23:42 +0100

    Locators for navBar, owners and pet

---
+++ helpers/owners_locators.ts
@@ -0,0 +1,17 @@
+export const ownersLocators = {
+  //owners
+  ownersTable: "table",
+  searchFilter: '[placeholder="Search Filter"]',
+  nameColumnCells: "table >> tr >> td:nth-child(1)", // Assuming Name is first column
+  firstNameInput: 'input[name="firstName"]',
+  lastNameInput: 'input[name="lastName"]',
+  addressInput: 'input[name="address"]',
+  cityInput: 'input[name="city"]',
+  telephoneInput: 'input[name="telephone"]',
+  submitButton: 'button[type="submit"]',
+  ownerLink: 'a:has-text("{ownerName}")', // Dynamic, replace {ownerName} in code
+
+  //owner information
+  addPetButton: "text=Add New Pet",
+  ownerInfoTitle: 'owner-details>h2:first-child',
+};
